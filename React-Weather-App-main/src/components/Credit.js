d35f6e05f02679510f745626e469e0db6de3cd6d
Tanamay singh <98833259+tanamaay@users.noreply.github.com>
2024-05-12 20:14:15 +0530

    Add files via upload

---
+++ React-Weather-App-main/src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href=""
+        >Tanamay Singh</a
+      >, <a href="https://github.com/tanamaay?tab=repositories">Open sourced on Github.</a> Hosted on Netlify
+    </p>
+  </div>
+  );
+}
